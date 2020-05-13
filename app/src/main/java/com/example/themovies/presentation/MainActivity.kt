c47f9c6c34edfaec230897e9b11e3e003efa87fb
rakapermanaptr <raka.putra@cxrus.com>
2020-05-13 15:23:39 +0700

    add some fragments and setup main activity

--- app/src/main/java/com/example/themovies/presentation/MainActivity.kt
+++ app/src/main/java/com/example/themovies/presentation/MainActivity.kt
@@ -2,12 +2,61 @@ package com.example.themovies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.themovies.R
+import com.example.themovies.presentation.discover.DiscoverFragment
+import com.example.themovies.presentation.nowplaying.NowPlayingFragment
+import com.example.themovies.presentation.profile.ProfileFragment
+import com.example.themovies.presentation.upcoming.UpcomingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.nav_discover -> {
+                    fragment = DiscoverFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_nowplaying -> {
+                    fragment = NowPlayingFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_upcoming -> {
+                    fragment = UpcomingFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_profile -> {
+                    fragment = ProfileFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            loadFragment(DiscoverFragment())
+        }
+
+        bttm_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, fragment)
+            .commit()
     }
 }
