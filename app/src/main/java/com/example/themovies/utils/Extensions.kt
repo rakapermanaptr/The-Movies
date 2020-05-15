996cc8dcc19e82a4688d8009d5ec6811b695b2a2
rakapermanaptr <raka.putra@cxrus.com>
2020-05-15 18:29:07 +0700

    create extension function for release date

--- app/src/main/java/com/example/themovies/utils/Extensions.kt
+++ app/src/main/java/com/example/themovies/utils/Extensions.kt
@@ -1,6 +1,9 @@
 package com.example.themovies.utils
 
 import android.view.View
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun View.show() {
     visibility = View.VISIBLE
@@ -8,4 +11,17 @@ fun View.show() {
 
 fun View.hide() {
     visibility = View.INVISIBLE
+}
+
+fun releaseDate(input: String): String {
+    val from = SimpleDateFormat("yyyy-MM-dd")
+    val to = SimpleDateFormat("dd MMM yyyy")
+    var date: Date? = null
+    return try {
+        date = from.parse(input)
+        to.format(date)
+    } catch (e: ParseException) {
+        e.printStackTrace()
+        input
+    }
 }
